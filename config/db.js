6ffd7249ad514f1110c74dc3103d5646bb804739
Xissamo <ximozed1@gmail.com>
2024-07-05 21:20:59 +0100

    localhost version 1.1

--- config/db.js
+++ config/db.js
@@ -1,19 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set.');
-}
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false, // Disable logging; default: console.log
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+const sequelize = new Sequelize('fileupload', 'fileuser', 'fileuserpassword', {
+    host: 'localhost',
+    dialect: 'postgres'
 });
 
 module.exports = { sequelize };
