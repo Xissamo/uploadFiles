c386307f1df114b07352c08c608d4c9c15789ae0
Xissamo <ximozed1@gmail.com>
2024-07-05 15:49:15 +0100

    db.js changed for render

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('fileupload', 'fileuser', 'fileuserpassword', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // Disable logging; default: console.log
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = { sequelize };
