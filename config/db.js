974be617674164da57a52c8ff5ad3e50a82614db
Xissamo <ximozed1@gmail.com>
2024-07-05 18:28:21 +0100

    Ensure DATABASE_URL is set and used correctly

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 const { Sequelize } = require('sequelize');
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set.');
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
